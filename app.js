127142e39c5c515574802b62ec2eb0fa7167a6e3
Brian Kimbrow <bkimbrow25@gmail.com>
2023-06-03 18:35:39 -0500

    newupdates

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let mainS = document.getElementById("main-s");
+let finalResult = document.getElementById ("final-Results");
+
+let mainS = document.getElementById("main-1";
+
+let finalResult = document.getElementById ("main-2");
+
+function oddDuck {
+    this.name = name;
+    this.src= img:
+    this.firstCount = 0;
+    this.finalCount = 0;
+}
