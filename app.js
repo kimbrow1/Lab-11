fc11f8b5e0bc2c6df26a1297233fe2652051dc8b
Brian Kimbrow <bkimbrow25@gmail.com>
2023-06-07 13:55:54 -0500

    newupdates

--- app.js
+++ app.js
@@ -3,13 +3,18 @@
 let mainS = document.getElementById("main-s");
 let finalResult = document.getElementById ("final-Results");
 
-let mainS = document.getElementById("main-1");
+let main1 = document.getElementById("main-1");
+let main2 = document.getElementById ("main-2");
+let main3 = document.getElementById ("main-3");
 
-let finalResult = document.getElementById ("main-2");
-
-function oddDuck {
+function oddDuck(name, img1) {
     this.name = name;
-    this.src= img;
+    this.img1 = img1;
     this.firstCount = 0;
     this.finalCount = 0;
 }
+
+let dogDuck = new oddDuck ("dogDuck", img/dog-duck.jpg);
+let sweep = new oddDuck ("sweep", img/sweep.png);
+let wine = new oddDuck ("wine", img/wine-glass.jpg);
+
